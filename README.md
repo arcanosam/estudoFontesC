e31c08c59d2c00c430d93f662908a62630b9b2fe
arcanosam <arcanosam@gmail.com>
2015-06-19 00:42:10 +0000

    formatação de código conforme gnu coding standards

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-  Desenvolvido no Debian. Rodado via linha de comando com GCC e GDB 
+Desenvolvido no Debian. Rodado via linha de comando com GCC e GDB 
 
-1) Pilha
-  minhapilha.c
-  Insere, pesquisa por valor, exclui, conta o tamanho da lista.
-  Usa malloc e ponteiro 
+_________________________________________________________________________
+
+1) Estrutura de dados: Pilha
+* Fonte: minhapilha.c
+  * Inserção
+  * Pesquisa por valor
+  * Exclusão
+  * Retorna tamanho da lista.
+  * Usa malloc e ponteiro 
 
   * para compilar:
     * gcc -g minhapilha.c -o pilha
@@ -11,20 +16,27 @@
   * para rodar:
     * se quiser usar gdb -> (sudo apt-get install gdb)
     * gdb ./pilha  ou somente ./pilha 
+
 _________________________________________________________________________
 
 2) Fork efetuado dia 16/06/2015
-Projeto importado do meu github para projeto no Eclipse CDT
+* Projeto importado do meu github para projeto no Eclipse CDT
 
 _________________________________________________________________________
 
 3) dia 17/06/2015 - Encontrado e corrigido 11 warnings
 
-* Inicializado as variaveis char com valor zero
-* Modificado nas funções do printf o especificador de formato de %u para %p
-* comentado a linha que declara a variavel command (pois nao é usada)
-* criado variavel para receber os retornos das funções system e scanf e ao fim imprimi-la no final apenas para evitar o warning de variavel não usada
+* Inicialização de variáveis char com valor zero
+* Especificador de formato %u das funções do printf modificados para %p
+* Variável 'command' não utilizada - linha comentada
+* Variável rs_scn criada para recepção dos retornos das funções: system e scanf
+  * Impressão do resultado desta variável ao final da execução do programa por mera conveniência afim de evitar warning
 
- 
+_________________________________________________________________________
 
-     
+4) dia 18/06/2015
+* Formatação de código
+  * Seguindo orientações do link: http://www.gnu.org/prep/standards/standards.html#Formatting
+  * TODO
+  	* incluir no momento da re-criação do menu a função 'system' chamando o comando 'clear' para limpar a telar
+  	* Mensagens informativas das opções escolhidas serem repassadas por parâmetro para serem impressas pela função criarMenu 
--- pilha/minhapilha.c
+++ pilha/minhapilha.c
@@ -2,219 +2,300 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct no {
+struct no
+{
 	int valor;
 	struct no *proximo;
 };
 
 
-struct no *adicionarNovoNo(struct no *node, int valor){
-        struct no *ptr;
+struct no *
+adicionarNovoNo (struct no *node, int valor)
+{
 
-	if (node == NULL){
-		printf("if\n");
-		node = malloc(sizeof(struct no));
+	struct no *ptr;
+
+	if (node == NULL)
+	  {
+		printf ("if\n");
+		node = malloc (sizeof (struct no));
 		node->valor = valor;
 		node->proximo = NULL;
-	} else {
-		printf("else\n");
-		ptr = malloc(sizeof(struct no));
+	  }
+	else
+	  {
+		printf ("else\n");
+		ptr = malloc (sizeof (struct no));
 		ptr->valor = valor;
 		ptr->proximo = node;
-                node = ptr;
-	}
+		node = ptr;
+	  }
 
-	printf(" valor %d - endereco %p \n",node->valor, node->proximo);
+	printf (" valor %d - endereco %p \n",node->valor, node->proximo);
 	return node;
 }
 
 
-void imprimir(struct no *node){
-        struct no *ptr;
+void
+imprimir (struct no *node)
+{
+	struct no *ptr;
 
-	if (node != NULL){
-                ptr = node;
-	  	printf(" valor: %d - meu ender: %p - prox: %p \n", ptr->valor, ptr, ptr->proximo);
-		
-		while (ptr->proximo){
-                   ptr = ptr->proximo;
-	  	   printf(" valor: %d - meu ender: %p - prox: %p \n",ptr->valor, ptr, ptr->proximo);
-		}
-	}
+	if (node != NULL)
+	  {
+		ptr = node;
+		printf (" valor: %d - meu ender: %p - prox: %p \n", ptr->valor, ptr, ptr->proximo);
+
+		while (ptr->proximo)
+		  {
+			ptr = ptr->proximo;
+			printf (" valor: %d - meu ender: %p - prox: %p \n", ptr->valor, ptr, ptr->proximo);
+		  }
+	  }
 } 
 
 
-int pesquisarPorPosicao(struct no *node, int pesquisa){
-        struct no *ptr, *proximo;
-        int posicao = -1;
-        int contador = 0;
+int
+pesquisarPorPosicao (struct no *node, int pesquisa)
+{
+	struct no *ptr, *proximo;
+	int posicao = -1;
+	int contador = 0;
 
-	if (node){
-                ptr = node;
+	if (node)
+	  {
+		ptr = node;
 		proximo = ptr;
 
-		while (proximo){
+		while (proximo)
+		  {
 
-                   if (ptr->valor != pesquisa) {
-                      ptr = ptr->proximo;
-                      proximo = ptr;
-                      contador++;
-                   } else {
-		     posicao = (contador +1);
-                     proximo = NULL;//para sair do loop
-		   }
+			 if (ptr->valor != pesquisa)
+			   {
+				ptr = ptr->proximo;
+				proximo = ptr;
+				contador++;
+			   }
+			 else
+			   {
+				 posicao = (contador +1);
+				 proximo = NULL;//para sair do loop
+			   }
+		  }
+	  }
 
-		}
-	}
-    return posicao;
+	return posicao;
 } 
 
-int contaLista(struct no *node) {
-  int ret = 0;
+int
+contaLista(struct no *node)
+{
+	int ret = 0;
+	struct no *ptr;
 
-  struct no *ptr;
-  ptr = node;
+	ptr = node;
 
-  while (ptr != NULL) {
-     ret++;
-     ptr = ptr->proximo;
-     //printf(" linha: %d  \n", ret);
-  }
+	while (ptr != NULL)
+	  {
+		ret++;
+		ptr = ptr->proximo;
+		//printf(" linha: %d  \n", ret);
+	  }
 
-  printf(" total de linhas na lista : %d  \n", ret);
-  return ret;
+	printf (" total de linhas na lista : %d  \n", ret);
+
+	return ret;
 }
 
-struct no *deletarNoPorValor(struct no *node, int pesquisa) {
-  int posicao = -1;
-  struct no *ptr = node;
+struct no *
+deletarNoPorValor(struct no *node, int pesquisa)
+{
+	int posicao = -1;
+	int contador = 0;
+	int totalLista;
+	struct no *ptr = node;
 
-  posicao = pesquisarPorPosicao(node, pesquisa);
 
-  int totalLista = contaLista(node);
+	posicao = pesquisarPorPosicao (node, pesquisa);
 
-  //diminuir 1 pq comeca contar de zero
-  posicao--;
+	totalLista = contaLista (node);
 
-  int contador = 0;
+	/* diminuir 1 pq comeca contar de zero */
+	posicao--;
 
-  //fazer o loop de acordo com a posicao, começando do valor zero.
-  //qdo encontrar tem que ligar o valor anterior ao posterior
-  if (posicao >= 0) {
-     //a solução para o primeiro elemento da lista
-     if (posicao == 0) {
-       //aponta o node para a posicao seguinte, pq o primeiro elemento é o a ser deletado
-       node = ptr->proximo;
-     } else if ((posicao+1) == totalLista) {
-        //a solução para excluir o ultimo elemento da lista 
-        //vai ate a posicao anterior à encontrada(q neste caso eh a penultima) -2 pq comecei a contar do zero
-        while (contador < (totalLista -2)){
-             node = node->proximo;
-	     printf(" valor: %d - meu ender: %p \n", node->valor,  node);
-             contador++;
-            //no final do loop chegara ao penultimo
-        }
+	/*
+	 * fazer o loop de acordo com a posicao, começando do valor zero.
+	 * qdo encontrar tem que ligar o valor anterior ao posterior
+	 */
+	if (posicao >= 0)
+	  {
+		/*
+		 * a solução para o primeiro elemento da lista
+		 */
+		if (posicao == 0)
+		  {
+			/*
+			 * aponta o node para a posicao seguinte,
+			 * pq o primeiro elemento é o a ser deletado
+			 */
+			node = ptr->proximo;
+		  }
+		else if ((posicao+1) == totalLista)
+		  {
+			/*
+			 * a solução para excluir o ultimo elemento da lista
+			 * vai ate a posicao anterior à encontrada(q neste caso eh a penultima)
+			 * -2 pq comecei a contar do zero
+			 */
+			while (contador < (totalLista -2))
+			  {
+				node = node->proximo;
+				printf (" valor: %d - meu ender: %p \n", node->valor,  node);
+				contador++;
 
-        //para excluir o ultimo
-        node->proximo = NULL; 
-        
-        //atualiza o ponteiro para o inicio da pilha
-        node = ptr;      	
-     } else {
-        //a solução para o elemento no meio da lista - para encontrar o elemento anterior ao ser deletado
-        ptr = node;
+			  }
 
-        while (ptr){
-	    if (contador == posicao-1) {
-	       //aponta para o proximo do elemento encontrado
-               ptr->proximo = ptr->proximo->proximo;//pega o posterior depois do excluido
-               break;
-            }
-	    contador++;	
-            ptr = ptr->proximo;
-	}
-     }
-  } else {
-	printf(" valor nao cadastrado na pilha. \n");
-  }
+			/*
+			 * no final do loop chegara ao penultimo
+			 */
 
-  return node;
+			/*
+			 * para excluir o ultimo
+			 */
+			node->proximo = NULL;
+
+			/*
+			 * atualiza o ponteiro para o inicio da pilha
+			 */
+			node = ptr;
+		  }
+		else
+		  {
+			/*
+			 * a solução para o elemento no meio da lista -
+			 * para encontrar o elemento anterior ao ser deletado
+			 */
+			ptr = node;
+
+			while (ptr)
+			  {
+				if (contador == posicao-1)
+				  {
+					/*
+					 * aponta para o proximo do elemento encontrado
+					 */
+					ptr->proximo = ptr->proximo->proximo; /* pega o posterior depois do excluido */
+					break;
+				  }
+
+				contador++;
+				ptr = ptr->proximo;
+			  }
+		  }
+	  }
+	else
+	  {
+		printf (" valor nao cadastrado na pilha. \n");
+	  }
+
+	return node;
 }
 
 void criarMenu(){
-	printf("\t 0 - Sair \n");
-	printf("\t 1 - Adicionar Elemento \n");
-	printf("\t 2 - Pesquisar por Elemento \n");
-	printf("\t 3 - Imprimir \n");
-	printf("\t 4 - Conta Lista \n");
-	printf("\t 5 - Deletar por Elemento \n");
-	printf("\n\n Escolha uma das opcoes acima =  ");
+	printf ("\t 0 - Sair \n");
+	printf ("\t 1 - Adicionar Elemento \n");
+	printf ("\t 2 - Pesquisar por Elemento \n");
+	printf ("\t 3 - Imprimir \n");
+	printf ("\t 4 - Conta Lista \n");
+	printf ("\t 5 - Deletar por Elemento \n");
+	printf ("\n\n Escolha uma das opcoes acima =  ");
 
 }
 
-int main(void)
+int main (void)
 {
 	// char command;
-	char ch=0, rank_ch=0;
-	struct no *node;
+	char ch=0;
+	char rank_ch=0;
 	int num, encontrado, rs_scn;
+	struct no *node;
 
-	rs_scn = system("clear");
+
+	rs_scn = system ("clear");
+
 	criarMenu();
 
-	rank_ch = getchar();
+	rank_ch = getchar ();
 
-        node = NULL;
-        while(ch !=  '0'){
-		switch (rank_ch) {
-	  	 case '0':  exit(EXIT_SUCCESS);
-	  	 case '1':           
-			printf("\n Entre com o valor :  ");
-			rs_scn = scanf("%d", &num);
-			node = adicionarNovoNo(node, num);
-                   break;
-	  	 case '2':           
-			printf("Entre com o valor para pesquisa:  ");
-			rs_scn = scanf("%d", &num);
+	node = NULL;
 
-			encontrado = pesquisarPorPosicao(node, num);
-	  	   	if (encontrado > 0){
-				printf(" valor encontrado na posicao: %d . \n",encontrado);
-			} else {
-				printf("Valor nao cadastrado.");
-			}	 
-			break;
-		case '3':           
-			imprimir(node); 
-			break;
-	  	 case '4':           
-			contaLista(node); 
-			break;
-	  	 case '5':           
-			printf("\n Entre com o valor :  ");
-			rs_scn = scanf("%d", &num);
-			
-			node = deletarNoPorValor(node, num); 
-			//deletarPorValor(node, num); 
+	while(ch !=  '0')
+	  {
+		switch (rank_ch)
+		  {
+			case '0':
+				exit(EXIT_SUCCESS);
+			case '1':
+				printf("\n Entre com o valor :  ");
+				rs_scn = scanf("%d", &num);
 
-			printf("\n  ");
-			printf("\n  Lista Atualizada.\n");
-			imprimir(node);
-			break;
-		}
+				node = adicionarNovoNo (node, num);
+				break;
+			case '2':
+				printf ("Entre com o valor para pesquisa:  ");
+				rs_scn = scanf ("%d", &num);
 
-		/**/
-                if (rank_ch !='\n') {
-			criarMenu();
-		}
-	
-		rank_ch = getchar();
-	
-    }
-    
-    printf("\n %d", rs_scn);
-    free(node);
+				encontrado = pesquisarPorPosicao (node, num);
 
-    return 0;
+				if (encontrado > 0)
+				  {
+					printf (" valor encontrado na posicao: %d . \n",encontrado);
+				  }
+				else
+				  {
+					printf ("Valor nao cadastrado.\n");
+				  }
+				break;
+			case '3':
+				imprimir (node);
+				break;
+			case '4':
+				contaLista (node);
+				break;
+			case '5':
+				printf ("\n Entre com o valor :  ");
+				rs_scn = scanf ("%d", &num);
+
+				node = deletarNoPorValor (node, num);
+				/*
+				 * deletarPorValor (node, num);
+				 */
+
+				printf ("\n  ");
+				printf ("\n  Lista Atualizada.\n");
+				imprimir (node);
+				break;
+		  }
+
+
+		if (rank_ch !='\n')
+		  {
+			/*
+			 * rs_scn = system ("clear");
+			 * TODO
+			 * passar mensagem referente a opção como parametro
+			 * da funcao criarMenu afim de nao perde-la apos a limpeza de tela
+			 */
+			criarMenu ();
+		  }
+
+		rank_ch = getchar ();
+
+	  }
+
+	printf ("\n %d", rs_scn);
+	free (node);
+
+	return 0;
 }
 
